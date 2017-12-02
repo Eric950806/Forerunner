e7a4bffcae3c452a9536a4a721444e4aa7ca6ed0
Eric950806 <eric288950806@gmail.com>
2017-12-02 18:00:12 +0800

    2017/12/2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ var studentsCollection = db.collection("students");
 
 $(document).ready(function(){
 	upDate(studentsCollection.find());
-	$("#table-tbody").on("click", ".col", colClick);
+	$("#table-tbody").on("click", ".dataID", colIDClick);
+	$("#table-tbody").on("click", ".btn-danger", btnDeleteClick);
 })
 
 
@@ -21,6 +22,24 @@ console.log(studentsCollection.find());
 
 
 studentsCollection.load(callback);
+
+
+function btnDeleteClick(){
+	upDate(studentsCollection.find());
+	if (!confirm("確定要刪除嗎?")) {return;}
+	var ID = $(this).closest("tr").find(".dataID").text();
+	studentsCollection.remove({
+    _id: ID
+	});
+	studentsCollection.save();
+}
+
+
+function datasave(){
+	console.log("data saved");
+	upDate(studentsCollection.find());
+}
+
 
 
 function callback(){
@@ -52,16 +71,19 @@ function upDate(datas){
 		$("#table-tbody").append(
 		"<tr class = 'col'>" +
 		"<td>" + (i+1) + "</td>" +
-		"<td class ='dateID'>" + datas[i]._id + "</td>" +
+		"<td class ='dataID'>" + datas[i]._id + "</td>" +
 		"<td>" + datas[i].name + "</td>" +
+		"<td><botton class = 'btn btn-warning'>修改</botton>" +
+		"<botton class='btn btn-danger'>刪除</botton></td>" +
 		"</tr>"
 		);
 	}
 }
 
 
-function colClick(){
-	var ID = $(this).find(".dateID").text();
+function colIDClick(){
+	console.log("colIDClick");
+	var ID = $(this).text();
 	var query = {
 	    _id: ID
 	};
@@ -73,6 +95,6 @@ function colClick(){
 		"<p>ID: " + studentData[0]._id + "</p>" + 
 		"<p>name: " + studentData[0].name + "</p>" + 
 		"<p>age: " + studentData[0].age + "</p>"
-		)
-	$("#mymodal").modal("show")
+		);
+	$("#mymodal").modal("show");
 }
